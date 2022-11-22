4eaf4fb88cc8e5585fb573f27f6264bd9510760c
Supamid44m <supamid780@gmail.com>
2022-11-22 19:01:59 +0700

    change

--- DemoSingleton.java
+++ DemoSingleton.java
@@ -1,11 +1,14 @@
 public class DemoSingleton{
     public static void main(String[] args) {
-        ///Moon a0 = new Moon();
+        ///Moon a0 = new Moon(); can't run
         Moon a1 = Moon.getInstance ();
         Moon a2 = Moon.getInstance();
         
         a1.showInfo();
 
         System.out.println(a1==a2);
+        
+        System.out.println(a1.hashCode());
+        System.out.println(a2.hashCode());
     }
 }
